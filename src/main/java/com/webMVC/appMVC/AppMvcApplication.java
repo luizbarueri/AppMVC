91f34e57acb72ebf664f43a268008beaff1409f7
Luiz Amaro <luizbarueri@gmail.com>
2024-02-06 14:06:51 -0300

    primeira pagina html estaica

---
+++ src/main/java/com/webMVC/appMVC/AppMvcApplication.java
@@ -0,0 +1,15 @@
+package com.webMVC.appMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMvcApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppMvcApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/webMVC/appMVC/AppMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webMVC.appMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
